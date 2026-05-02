00ba1c920d359236d39475f0bf92d950c940f307
4shanemo-wq <4shanemo@gmail.com>
2026-05-01 19:56:31 -0700

    Update gallery-data.js

--- RC APP/assets/gallery-data.js
+++ RC APP/assets/gallery-data.js
@@ -3,85 +3,85 @@
     source: "assets/Gallery/2026-04-19_12.19.40.png",
     full: "assets/Gallery/display/2026-04-19_12.19.40.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.19.40.jpg",
-    alt: "RuneCraft build highlight 1",
-    caption: "RuneCraft build highlight 1"
+    alt: "Hail'z Runecraft base",
+    caption: "Hail'z Runecraft base"
   },
   {
     source: "assets/Gallery/2026-04-19_12.19.59.png",
     full: "assets/Gallery/display/2026-04-19_12.19.59.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.19.59.jpg",
-    alt: "RuneCraft build highlight 2",
-    caption: "RuneCraft build highlight 2"
+    alt: "Googletopia",
+    caption: "Googletopia"
   },
   {
     source: "assets/Gallery/2026-04-19_12.20.48.png",
     full: "assets/Gallery/display/2026-04-19_12.20.48.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.20.48.jpg",
-    alt: "RuneCraft build highlight 3",
-    caption: "RuneCraft build highlight 3"
+    alt: "Alphiee's Runecraft base",
+    caption: "Alphiee's Runecraft base"
   },
   {
     source: "assets/Gallery/2026-04-19_12.21.16.png",
     full: "assets/Gallery/display/2026-04-19_12.21.16.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.21.16.jpg",
-    alt: "RuneCraft build highlight 4",
-    caption: "RuneCraft build highlight 4"
+    alt: "Kooby's Runecraft base",
+    caption: "Kooby's Runecraft base"
   },
   {
     source: "assets/Gallery/2026-04-19_12.21.33.png",
     full: "assets/Gallery/display/2026-04-19_12.21.33.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.21.33.jpg",
-    alt: "RuneCraft build highlight 5",
-    caption: "RuneCraft build highlight 5"
+    alt: "Cutie animals",
+    caption: "Cutie animals"
   },
   {
     source: "assets/Gallery/2026-04-19_12.22.02.png",
     full: "assets/Gallery/display/2026-04-19_12.22.02.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.22.02.jpg",
-    alt: "RuneCraft build highlight 6",
-    caption: "RuneCraft build highlight 6"
+    alt: "Spawn / Shopping district",
+    caption: "Spawn / Shopping district"
   },
   {
     source: "assets/Gallery/2026-04-19_12.22.58.png",
     full: "assets/Gallery/display/2026-04-19_12.22.58.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.22.58.jpg",
-    alt: "RuneCraft build highlight 7",
-    caption: "RuneCraft build highlight 7"
+    alt: "DK's Runecraft base",
+    caption: "DK's Runecraft base"
   },
   {
     source: "assets/Gallery/2026-04-19_12.23.38.png",
     full: "assets/Gallery/display/2026-04-19_12.23.38.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.23.38.jpg",
-    alt: "RuneCraft build highlight 8",
-    caption: "RuneCraft build highlight 8"
+    alt: "Jerry's Runecraft base",
+    caption: "Jerry's Runecraft base"
   },
   {
     source: "assets/Gallery/2026-04-19_12.24.05.png",
     full: "assets/Gallery/display/2026-04-19_12.24.05.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.24.05.jpg",
-    alt: "RuneCraft build highlight 9",
-    caption: "RuneCraft build highlight 9"
+    alt: "Frizzle's Runecraft base",
+    caption: "Frizzle's Runecraft base"
   },
   {
     source: "assets/Gallery/2026-04-19_12.24.49.png",
     full: "assets/Gallery/display/2026-04-19_12.24.49.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.24.49.jpg",
-    alt: "RuneCraft build highlight 10",
-    caption: "RuneCraft build highlight 10"
+    alt: "Seth's Runecraft base",
+    caption: "Seth's Runecraft base"
   },
   {
     source: "assets/Gallery/2026-04-19_12.25.17.png",
     full: "assets/Gallery/display/2026-04-19_12.25.17.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-19_12.25.17.jpg",
-    alt: "RuneCraft build highlight 11",
-    caption: "RuneCraft build highlight 11"
+    alt: "Emma's Runecraft base",
+    caption: "Emma's Runecraft base"
   },
   {
     source: "assets/Gallery/2026-04-17_19.47.32.png",
     full: "assets/Gallery/display/2026-04-17_19.47.32.jpg",
     thumb: "assets/Gallery/thumbs/2026-04-17_19.47.32.jpg",
-    alt: "RuneCraft build highlight 12",
-    caption: "RuneCraft build highlight 12"
+    alt: "???",
+    caption: "???"
   }
 ];
 
